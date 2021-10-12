f6c44d5fbc3152ed7d3e8ff9b9a4423a1d99a053
roustr <artem-_96@mail.ru>
2021-10-12 10:34:56 +0300

    Корректировка компонента ms-birthday. Удалил иконку пользователя. Вынес стили меню из родительского блока в общую структуру

--- MainSite/Scripts/Components/Birthday/ms-birthday.vue
+++ MainSite/Scripts/Components/Birthday/ms-birthday.vue
@@ -1,108 +1,111 @@
 ﻿<template>
-    <div v-if="users && users.length" class="card-panel card_birthday">
-        <div class="card_birthday-title bold">
-            C днём рождения!
+  <div class="card_birthday">
+    <div v-if="users && users.length" class="card-panel">
+      <div class="card_birthday-title bold">C днём рождения!</div>
+      <div class="card_birthday-content">
+        <div class="card_birthday-content_description" v-for="item in users" :key="item.Id">
+          <span class="fio">{{ item.FIO }}</span>
+          <span class="subdivision">{{ getSubDivision(item) }}</span>
         </div>
-        <div class="card_birthday-content">
-            <div class="card_birthday-content_description"
-                 v-for="item in users"
-                 :key="item.Id">
-                <span class="fio">{{item.FIO}}</span>
-                <span class="subdivision">{{getSubDivision(item)}}</span>
-            </div>
-        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios'
+  import axios from 'axios';
 
-    export default {
-        data() {
-            return {
-                users: []
-            }
-        },
-        methods: {
-            setUsers() {
-                axios('/api/ApiUsers/GetBirthdayUsers', {
-                    method: 'GET'
-                })
-                .then(responce => {
-                    this.users = responce.data;       
-                });
-            },
-            getSubDivision(subDivision) {
-                if (typeof subDivision == 'undefined' || subDivision == null) return 'ООНРиПНПК';
+  export default {
+    data() {
+      return {
+        users: [],
+      };
+    },
+    methods: {
+      setUsers() {
+        axios('/api/ApiUsers/GetBirthdayUsers', {
+          method: 'GET',
+        }).then((responce) => {
+          this.users = responce.data;
+        });
+      },
+      getSubDivision(subDivision) {
+        if (typeof subDivision == 'undefined' || subDivision == null) return 'ООНРиПНПК';
 
-                return subDivision.DepartmentShortName !== '' ? subDivision.DepartmentShortName : subDivision.DepartmentFullName
-            }
-        },
-        mounted() {
-            this.setUsers();
-        }
-    };
+        return subDivision.DepartmentShortName !== ''
+          ? subDivision.DepartmentShortName
+          : subDivision.DepartmentFullName;
+      },
+    },
+    mounted() {
+      this.setUsers();
+    },
+  };
 </script>
 
 <style lang="scss">
-     /*Оформление блока день рождение(birthday)*/
-    .card_birthday {
-        position: absolute;
-        right: 0;
-        top: 0;
-        margin: 0;
-        transform: translateX(110%);
-        width: 25%;
-
-        @media (max-width: 900px) {
-            margin: 0.5rem 0 1rem 0;
-            transform: translateX(0px);
-            position: inherit;
-            width: 100%;
-        }
-
-        &-title {
-            text-align: center;
-            color:#B12344;
-            padding-bottom: 5px;
-            @media (max-width: 1400px) {
-                text-align: center;
-                padding-bottom: 15px;
-                width: 100%;
-            }
-        }
-
-        &-content {
-            font-size: 14px;
-            display:flex;
-            flex-direction:column;
-            @media (max-width: 1400px) {
-                flex-direction: row;
-                flex-wrap: wrap;
-                justify-content: inherit !important;
-            }
-            &_description {
-                display:flex;
-                flex-wrap:wrap;               
-                @media (max-width: 1400px) {
-                    padding:5px;
-                }
-                .fio {
-                   padding-right: 10px;
-                   font-weight: 600;
-                   /*flex-basis:70%;*/
-                    @media (max-width: 1400px) {
-                        flex-basis:inherit;
-                    }
-                }
-                .subdivision {
-                   font-style: italic;
-                   /*flex-basis:30%;*/
-                   @media (max-width: 1400px) {
-                        flex-basis:inherit;
-                    }
-                }
-            }
-        }
+  /*Оформление блока день рождение(birthday)*/
+  .card_birthday {
+    .card-panel {
+      margin-top: 0px;
     }
+
+    position: absolute;
+    right: 0;
+    top: 0;
+    margin: 0;
+    transform: translateX(110%);
+    width: 25%;
+
+    @media (max-width: 900px) {
+      margin: 0.5rem 0 1rem 0;
+      transform: translateX(0px);
+      position: inherit;
+      width: 100%;
+    }
+
+    &-title {
+      text-align: center;
+      color: #b12344;
+      padding-bottom: 5px;
+      @media (max-width: 1400px) {
+        text-align: center;
+        padding-bottom: 15px;
+        width: 100%;
+      }
+    }
+
+    &-content {
+      font-size: 14px;
+      display: flex;
+      flex-direction: column;
+      @media (max-width: 1400px) {
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: inherit !important;
+      }
+      &_description {
+        display: flex;
+        flex-wrap: wrap;
+        @media (max-width: 1400px) {
+          padding: 5px;
+        }
+        .fio {
+          padding-right: 10px;
+          font-weight: 600;
+          /*flex-basis:70%;*/
+          @media (max-width: 1400px) {
+            flex-basis: inherit;
+          }
+        }
+        .subdivision {
+          font-style: italic;
+          /*flex-basis:30%;*/
+          @media (max-width: 1400px) {
+            flex-basis: inherit;
+          }
+        }
+      }
+    }
+  }
 </style>
--- MainSite/Scripts/Components/Header/ms-header.vue
+++ MainSite/Scripts/Components/Header/ms-header.vue
@@ -1,173 +1,182 @@
 ﻿<template>
-    <header class="navbar-fixed">
-        <nav class="nav-header">
-            <div class="container">
-                <div class="nav-wrapper">
-                    <div class="valign-wrapper">
-                        <a style="cursor: pointer;" @click="routerPushMainView" class="valign-wrapper">
-                            <img :src="GetApplicationIcon" width="50" height="50" /> 
-                            <span 
-                              class="bold" 
-                              style="padding-left:10px;line-height: 15px;margin-right: 13px;font-size:12px;"
-                              v-html="GetApplicationName"
-                            ></span>
-                        </a>
-                        <a id="openMenu" data-target="mobile-demo" class="sidenav-trigger" style="float: right;cursor:pointer;"><i class="material-icons">menu</i></a>
-                        <div class="hide-on-med-and-down col m12 s12 l9 headerMenu">
-                            <div class="col l6 headerMenu-search">
-                                <span class="bold">Поиск:</span>
-                                <input v-model="searchText" class="inputTextMainSite" type="text" />
-                                <button class="btn btn-default" @click="searchNews">Найти</button>
-                            </div>
-                            <a data-target='dropdown1' class="dropdown-trigger valign-wrapper" style="justify-content:flex-end;">
-                                <span class="headerMenu-user__info">{{currentUser.Name}}</span>
-                                <img src="/images/layout_icons/userLogout.svg" alt="" />
-                            </a>
-                            <!--<ul id='dropdown1' class='dropdown-content headerMenu-user__settings'>
-                                <li><a href="#!"><i class="material-icons">home</i>Личный кабинет</a></li>
-                                <li><a href="#!"><i class="material-icons">cloud</i>Управление сервисами</a></li>
-                            </ul>-->
-                        </div>
-                    </div>
-                </div>
+  <header class="navbar-fixed">
+    <nav class="nav-header">
+      <div class="container">
+        <div class="nav-wrapper">
+          <div class="valign-wrapper">
+            <a style="cursor: pointer" @click="routerPushMainView" class="valign-wrapper">
+              <img :src="GetApplicationIcon" width="50" height="50" />
+              <span
+                class="bold"
+                style="padding-left: 10px; line-height: 15px; margin-right: 13px; font-size: 12px"
+                v-html="GetApplicationName"
+              ></span>
+            </a>
+            <a
+              id="openMenu"
+              data-target="mobile-demo"
+              class="sidenav-trigger"
+              style="float: right; cursor: pointer"
+              ><i class="material-icons">menu</i></a
+            >
+            <div class="hide-on-med-and-down col m12 s12 l9 headerMenu">
+              <div class="col l6 headerMenu-search">
+                <span class="bold">Поиск:</span>
+                <input v-model="searchText" class="inputTextMainSite" type="text" />
+                <button class="btn btn-default" @click="searchNews">Найти</button>
+              </div>
+              <a
+                data-target="dropdown1"
+                class="dropdown-trigger valign-wrapper"
+                style="justify-content: flex-end"
+              >
+                <span class="headerMenu-user__info">{{ currentUser.Name }}</span>
+                <!--<img src="/images/layout_icons/userLogout.svg" alt="" />-->
+              </a>
+              <!--<ul id='dropdown1' class='dropdown-content headerMenu-user__settings'>
+								<li><a href="#!"><i class="material-icons">home</i>Личный кабинет</a></li>
+								<li><a href="#!"><i class="material-icons">cloud</i>Управление сервисами</a></li>
+							</ul>-->
             </div>
-            <div class="progress" id="progressLoad" style="background-color:transparent; margin:0px;" v-bind:style="isShow[isActive]">
-                <div class="indeterminate" style="background-color:#64b5f6;"></div>
-            </div>
-        </nav>
-    </header>
+          </div>
+        </div>
+      </div>
+      <div
+        class="progress"
+        id="progressLoad"
+        style="background-color: transparent; margin: 0px"
+        v-bind:style="isShow[isActive]"
+      >
+        <div class="indeterminate" style="background-color: #64b5f6"></div>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
-    import { mapState, mapActions, mapMutations } from 'vuex';
+  import { mapState, mapActions, mapMutations } from 'vuex';
 
-    export default {
-        name: 'ms-header',
-        data() {
-            return {
-                userName: 'Незарегистрированный пользователь',
-                searchText: '',
-                isShow: {
-                    true: { display: 'block' },
-                    false: {display : 'none'}
-                }
-            }
+  export default {
+    name: 'ms-header',
+    data() {
+      return {
+        userName: 'Незарегистрированный пользователь',
+        searchText: '',
+        isShow: {
+          true: { display: 'block' },
+          false: { display: 'none' },
         },
-        computed: {
-            ...mapState('settings', ['settings']),
-            ...mapState('user', ['currentUser']),
-            ...mapState('preLoader', ['isActive']),
-            GetApplicationName() {
-                return this.searchSettingByName("Application.Name", "WebSite");
-            },
-            GetApplicationIcon() {
-                return this.searchSettingByName("Application.Icon", "/images/layout_icons/header.png");
-            }
-        },
-        methods: {
-            ...mapActions('settings', ['GET_SETTINGS']),
-            ...mapActions('user', ['GET_INFO_BY_CURRENT_USER']),
-            ...mapMutations('menu', ['SET_OR_UPDATE_ACTIVE_CATEGORY']),
-            searchNews() {
-                if (Object.keys(this.$route.params).length == 0 || this.searchText != '') {
-                    let routerParams = { name: 'search', params: { searchText: this.searchText } };
-                    
-                    if(this.$route.params.searchText != this.searchText) {
-                        this.$router.push(routerParams);
-                    }
-                }
-            },
-            searchSettingByName(name, defaultName) {
+      };
+    },
+    computed: {
+      ...mapState('settings', ['settings']),
+      ...mapState('user', ['currentUser']),
+      ...mapState('preLoader', ['isActive']),
+      GetApplicationName() {
+        return this.searchSettingByName('Application.Name', 'WebSite');
+      },
+      GetApplicationIcon() {
+        return this.searchSettingByName('Application.Icon', '/images/layout_icons/header.png');
+      },
+    },
+    methods: {
+      ...mapActions('settings', ['GET_SETTINGS']),
+      ...mapActions('user', ['GET_INFO_BY_CURRENT_USER']),
+      ...mapMutations('menu', ['SET_OR_UPDATE_ACTIVE_CATEGORY']),
+      searchNews() {
+        if (Object.keys(this.$route.params).length == 0 || this.searchText != '') {
+          let routerParams = { name: 'search', params: { searchText: this.searchText } };
 
-                let item = this.settings.find(function (item) {
-                    if (item.Name == name && item.Value != '') {
-                        return item;
-                    }
-                });
-
-                return typeof item == 'undefined' || item == null ? defaultName : item.Value
-
-            },
-            routerPushMainView() {
-                if (this.$route.name != 'main' ) {
-                    this.SET_OR_UPDATE_ACTIVE_CATEGORY(null);
-                    this.$router.push('/');
-                }
-            }
-        },
-        mounted() {
-            this.GET_SETTINGS();
-            this.GET_INFO_BY_CURRENT_USER();
-        },
-        created() {
-            this.$questionHub.connetionAdminOpened()
-        },
-        beforeDestroy() {
-            this.$questionHub.connetionAdminClosed()
+          if (this.$route.params.searchText != this.searchText) {
+            this.$router.push(routerParams);
+          }
         }
-    }
+      },
+      searchSettingByName(name, defaultName) {
+        let item = this.settings.find(function (item) {
+          if (item.Name == name && item.Value != '') {
+            return item;
+          }
+        });
+
+        return typeof item == 'undefined' || item == null ? defaultName : item.Value;
+      },
+      routerPushMainView() {
+        if (this.$route.name != 'main') {
+          this.SET_OR_UPDATE_ACTIVE_CATEGORY(null);
+          this.$router.push('/');
+        }
+      },
+    },
+    mounted() {
+      this.GET_SETTINGS();
+      this.GET_INFO_BY_CURRENT_USER();
+    },
+    created() {
+      this.$questionHub.connetionAdminOpened();
+    },
+    beforeDestroy() {
+      this.$questionHub.connetionAdminClosed();
+    },
+  };
 </script>
 
 <style lang="scss">
-    .isActive {
-        display:block;
+  .isActive {
+    display: block;
+  }
+
+  .secondMenu {
+    display: flex;
+    padding-left: 0px;
+    padding-right: 0px;
+
+    &:last-child {
+      display: flex;
+      align-items: center;
+      margin-left: auto;
+    }
+  }
+
+  .headerMenu {
+    padding: 0 !important;
+
+    &-search {
+      display: flex;
+      align-items: center;
+      padding-left: 0px !important;
+      margin-left: 0;
+
+      & > span {
+        margin-right: 5px;
+      }
+
+      & > .inputTextMainSite {
+        margin-right: 5px;
+        max-width: calc(100% - 150px);
+        border-radius: 8px 0px 0px 8px !important;
+        border-right: none !important;
+      }
+
+      & > .btn-default {
+        border-radius: 0px 8px 8px 0px !important;
+      }
     }
 
-    .secondMenu {
-        display: flex;
-        padding-left: 0px;
-        padding-right: 0px;
-
-        &:last-child {
-            display: flex;
-            align-items: center;
-            margin-left: auto;
-        }
+    & > li {
+      &:last-child {
+        margin-left: auto;
+      }
     }
 
-    .headerMenu {
-        padding: 0 !important;
+    &-user {
+      padding: 0;
+      &__info {
+        font-size: 14px;
+      }
 
-        &-search {
-            display: flex;
-            align-items: center;
-            padding-left: 0px !important;
-            margin-left: 0;
-
-            & > span {
-                margin-right:5px;
-            }
-
-            & > .inputTextMainSite{
-                margin-right:5px;
-                max-width: calc( 100% - 150px);
-                border-radius: 8px 0px 0px 8px !important;
-                border-right:none !important;
-            }
-
-            & > .btn-default {
-                border-radius: 0px 8px 8px 0px !important;
-            }
-        }
-
-        & > li {
-
-            &:last-child {
-                margin-left: auto;
-            }
-
-        }
-
-        &-user {
-            padding:0;
-            &__info {
-                font-size:14px;
-                padding-right:10px;
-            }
-            
-            &__settings {
-
-            }
-        }
+      &__settings {
+      }
     }
+  }
 </style>
