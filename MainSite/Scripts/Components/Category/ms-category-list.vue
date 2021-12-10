29d727806908dfe2bdac6df294c629c0acb84b86
roustr <artem-_96@mail.ru>
2021-12-10 10:17:25 +0300

    правки по верстке блока контейнера(меню и главный блок с контентом).

--- MainSite/Scripts/Components/Category/ms-category-list.vue
+++ MainSite/Scripts/Components/Category/ms-category-list.vue
@@ -2,8 +2,16 @@
   <div class="row" style="margin-top: 10px">
     <msBreadCrumbs />
     <div class="flexCategoryList">
-      <msCategoryItem v-for="item in getCategoryChildren" :key="item.Id" :category_item="item" />
-      <div class="card-panel ms-category-add_item" @click="createCategory" v-if="isEditer">
+      <msCategoryItem
+        v-for="item in getCategoryChildren"
+        :key="item.Id"
+        :category_item="item"
+      />
+      <div
+        class="card-panel ms-category-add_item"
+        @click="createCategory"
+        v-if="isEditer"
+      >
         <h6 class="text-center">Создать новый подраздел...</h6>
         <div class="text-center">
           <i class="material-icons large"> note_add </i>
@@ -14,112 +22,114 @@
 </template>
 
 <script>
-  import msCategoryItem from '../Category/ms-category-item.vue';
-  import msBreadCrumbs from '../../DefaultComponents/ms-breadcrumbs-category.vue';
-  import msCategoryAddItem from '../Category/ms-category-item_create.vue';
-  import { mapActions, mapState } from 'vuex';
-  export default {
-    props: {
-      category: {
-        type: Object,
-        default: () => {
-          return {};
-        },
+import msCategoryItem from "../Category/ms-category-item.vue";
+import msBreadCrumbs from "../../DefaultComponents/ms-breadcrumbs-category.vue";
+import msCategoryAddItem from "../Category/ms-category-item_create.vue";
+import { mapActions, mapState } from "vuex";
+export default {
+  props: {
+    category: {
+      type: Object,
+      default: () => {
+        return {};
       },
     },
-    data() {
-      return {
-        isEditer: false,
-      };
-    },
-    components: {
-      msCategoryItem,
-      msBreadCrumbs,
-      msCategoryAddItem,
-    },
-    watch: {
-      $route: 'fetchData',
-    },
-    computed: {
-      ...mapState('menu', ['breadcrumbs', 'categories']),
-      getCategoryChildren() {
-        if (!this.$route.params.categoryId) return [];
-        let children = [];
+  },
+  data() {
+    return {
+      isEditer: false,
+    };
+  },
+  components: {
+    msCategoryItem,
+    msBreadCrumbs,
+    msCategoryAddItem,
+  },
+  watch: {
+    $route: "fetchData",
+  },
+  computed: {
+    ...mapState("menu", ["breadcrumbs", "categories"]),
+    getCategoryChildren() {
+      if (!this.$route.params.categoryId) return [];
+      let children = [];
 
-        if (Object.keys(this.category).length == 0) {
-          children = this.getCategoryById(this.$route.params.categoryId);
-        } else {
-          children = this.category.Children;
-        }
+      if (Object.keys(this.category).length == 0) {
+        children = this.getCategoryById(this.$route.params.categoryId);
+      } else {
+        children = this.category.Children;
+      }
 
-        return children;
-      },
+      return children;
     },
-    methods: {
-      ...mapActions('menu', ['GET_CATEGORIES_BY_BREADCRUMBS']),
-      ...mapActions('user', ['GET_PERMISSION_BY_CATEGORY']),
-      fetchData() {
-        this.getInfoByPermission();
-        //this.GET_CATEGORIES_BY_BREADCRUMBS(this.$route.params.categoryId);
-      },
-      getCategoryById(categoryId) {
-        if (!this.categories.length) return [];
-
-        let set = new Set();
-        function arrMaper(arr) {
-          arr.forEach((element) => {
-            if (element != null) {
-              if (element.Children !== []) arrMaper(element.Children);
-              set.add(element);
-            }
-          });
-        }
-        arrMaper(this.categories.slice());
-        let categoryes = Array.from(set).find(function (item) {
-          if (item.Id == categoryId) return item;
-        });
-
-        return Object.keys(categoryes).length == 0 ? [] : categoryes.Children;
-      },
-      createCategory() {
-        let parentCategoryName =
-          this.breadcrumbs && this.breadcrumbs.length > 0
-            ? this.breadcrumbs[this.breadcrumbs.length - 1].name
-            : '';
-        this.$router.push({
-          name: 'createCategory',
-          params: {
-            parentCategoryId: this.$route.params.categoryId,
-            parentCategoryName: parentCategoryName,
-          },
-        });
-      },
-      async getInfoByPermission() {
-        this.isEditer = await this.GET_PERMISSION_BY_CATEGORY(this.$route.params.categoryId);
-      },
-    },
-    created() {
+  },
+  methods: {
+    ...mapActions("menu", ["GET_CATEGORIES_BY_BREADCRUMBS"]),
+    ...mapActions("user", ["GET_PERMISSION_BY_CATEGORY"]),
+    fetchData() {
       this.getInfoByPermission();
+      //this.GET_CATEGORIES_BY_BREADCRUMBS(this.$route.params.categoryId);
     },
-  };
+    getCategoryById(categoryId) {
+      if (!this.categories.length) return [];
+
+      let set = new Set();
+      function arrMaper(arr) {
+        arr.forEach((element) => {
+          if (element != null) {
+            if (element.Children !== []) arrMaper(element.Children);
+            set.add(element);
+          }
+        });
+      }
+      arrMaper(this.categories.slice());
+      let categoryes = Array.from(set).find(function (item) {
+        if (item.Id == categoryId) return item;
+      });
+
+      return Object.keys(categoryes).length == 0 ? [] : categoryes.Children;
+    },
+    createCategory() {
+      let parentCategoryName =
+        this.breadcrumbs && this.breadcrumbs.length > 0
+          ? this.breadcrumbs[this.breadcrumbs.length - 1].name
+          : "";
+      this.$router.push({
+        name: "createCategory",
+        params: {
+          parentCategoryId: this.$route.params.categoryId,
+          parentCategoryName: parentCategoryName,
+        },
+      });
+    },
+    async getInfoByPermission() {
+      this.isEditer = await this.GET_PERMISSION_BY_CATEGORY(
+        this.$route.params.categoryId
+      );
+    },
+  },
+  created() {
+    this.getInfoByPermission();
+  },
+};
 </script>
 
 <style scoped lang="scss">
-  .ms-category-add_item {
-    cursor: pointer;
-    margin: 10px 10px;
-    width: 47%;
-    height: 240px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
+.ms-category-add_item {
+  cursor: pointer;
+  margin: 10px 10px;
+  width: 46%;
+  height: 240px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
 
-  .flexCategoryList {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    margin: 0 -10px;
-    align-items: stretch;
-  }
+.flexCategoryList {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 0 -10px;
+  align-items: stretch;
+}
 </style>
--- MainSite/Scripts/Components/Menu/ms-menu.vue
+++ MainSite/Scripts/Components/Menu/ms-menu.vue
@@ -1,53 +1,69 @@
 ﻿<template>
-  <ul class="menu">
-    <li>
-      <router-link :to="{ name: 'main' }" :class="{ active: $route.path === '/' }">
-        <img src="/images/layout_icons/newsfeed.svg" />
-        <div class="bold">Новости</div>
-      </router-link>
-    </li>
-    <ms-menu-item v-for="category in categories" :key="category.id" :menu_item="category">
-    </ms-menu-item>
-  </ul>
+  <div class="col m4 s12 l3" id="menuBlock">
+    <ul class="menu">
+      <li>
+        <router-link
+          :to="{ name: 'main' }"
+          :class="{ active: $route.path === '/' }"
+        >
+          <img src="/images/layout_icons/newsfeed.svg" />
+          <div class="bold">Новости</div>
+        </router-link>
+      </li>
+      <ms-menu-item
+        v-for="category in categories"
+        :key="category.id"
+        :menu_item="category"
+      >
+      </ms-menu-item>
+    </ul>
+  </div>
 </template>
 
 <script>
-  import msMenuItem from './ms-menu-item.vue';
-  import { mapActions, mapState, mapMutations } from 'vuex';
+import msMenuItem from "./ms-menu-item.vue";
+import { mapActions, mapState, mapMutations } from "vuex";
 
-  export default {
-    name: 'ms-menu',
-    data() {
-      return {
-        categoryId: undefined,
-      };
-    },
-    components: {
-      msMenuItem,
-    },
-    computed: {
-      ...mapState('menu', ['categories', 'breadcrumbs']),
-    },
-    methods: {
-      ...mapActions('menu', ['GET_CATEGORIES']),
-      searchSettingByName(name, defaultName) {
-        let item = this.settings.find(function (item) {
-          if (item.Name == name && item.Value != '') {
-            return item;
-          }
-        });
+export default {
+  name: "ms-menu",
+  data() {
+    return {
+      categoryId: undefined,
+    };
+  },
+  components: {
+    msMenuItem,
+  },
+  computed: {
+    ...mapState("menu", ["categories", "breadcrumbs"]),
+  },
+  methods: {
+    ...mapActions("menu", ["GET_CATEGORIES"]),
+    searchSettingByName(name, defaultName) {
+      let item = this.settings.find(function (item) {
+        if (item.Name == name && item.Value != "") {
+          return item;
+        }
+      });
 
-        return typeof item == 'undefined' || item == null ? defaultName : item.Value;
-      },
+      return typeof item == "undefined" || item == null
+        ? defaultName
+        : item.Value;
     },
-    created() {
-      this.GET_CATEGORIES();
-    },
-  };
+  },
+  created() {
+    this.GET_CATEGORIES();
+  },
+};
 </script>
 
 <style scoped lang="scss">
-  .rectangle {
-    background-color: white;
+@media only screen and (min-width: 883px) and (max-width: 1300px) {
+  #menuBlock {
+    width: 33.33333%;
   }
+}
+.rectangle {
+  background-color: white;
+}
 </style>
